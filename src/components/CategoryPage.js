2eba486ec82891c0dc971cb57582679d0e0a73b4
Sajad Sajadpour <sajad.sajadpour@gmail.com>
2024-06-06 13:23:02 +0800

    fix issue for images

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -4,47 +4,51 @@ import { Link, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
+import mango from '../Images/mango.jpeg';
+import cheesecake from '../Images/cheesecake.jpeg';
+import shisha from '../Images/image123.jpeg';
+import snacks from '../Images/snacks.jpeg';
 const categoryItems = {
   drinks: [
-    { name: 'Drink 1', price: '$5', image: '/images/mango.jpeg' },
-    { name: 'Drink 2', price: '$6', image: '/images/mango.jpeg' },
-    { name: 'Drink 3', price: '$7', image: '/images/mango.jpeg' },
-    { name: 'Drink 3', price: '$7', image: '/images/mango.jpeg' },
-    { name: 'Drink 3', price: '$7', image: '/images/mango.jpeg' },
-    { name: 'Drink 3', price: '$7', image: '/images/mango.jpeg' },
-    { name: 'Drink 3', price: '$7', image: '/images/mango.jpeg' },
-    { name: 'Drink 3', price: '$7', image: '/images/mango.jpeg' },
+    { name: 'Drink 1', price: '$5', image: mango },
+    { name: 'Drink 2', price: '$6', image: mango },
+    { name: 'Drink 3', price: '$7', image: mango },
+    { name: 'Drink 3', price: '$7', image: mango },
+    { name: 'Drink 3', price: '$7', image: mango },
+    { name: 'Drink 3', price: '$7', image: mango },
+    { name: 'Drink 3', price: '$7', image: mango },
+    { name: 'Drink 3', price: '$7', image: mango },
   ],
   shisha: [
-    { name: 'Shisha 1', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 2', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 3', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 4', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 5', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 6', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 7', price: '$20', image: '/images/image123.jpeg' },
-    { name: 'Shisha 8', price: '$20', image: '/images/image123.jpeg' },
+    { name: 'Shisha 1', price: '$20', image: shisha },
+    { name: 'Shisha 2', price: '$20', image: shisha },
+    { name: 'Shisha 3', price: '$20', image: shisha },
+    { name: 'Shisha 4', price: '$20', image: shisha },
+    { name: 'Shisha 5', price: '$20', image: shisha },
+    { name: 'Shisha 6', price: '$20', image: shisha },
+    { name: 'Shisha 7', price: '$20', image: shisha },
+    { name: 'Shisha 8', price: '$20', image: shisha },
   ],
   desserts: [
-    { name: 'Dessert 1', price: '$8', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 2', price: '$9', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 3', price: '$7', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 4', price: '$8', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 5', price: '$9', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 6', price: '$7', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 7', price: '$9', image: '/images/cheesecake.jpeg' },
-    { name: 'Dessert 8', price: '$7', image: '/images/cheesecake.jpeg' },
+    { name: 'Dessert 1', price: '$8', image: cheesecake },
+    { name: 'Dessert 2', price: '$9', image: cheesecake },
+    { name: 'Dessert 3', price: '$7', image: cheesecake },
+    { name: 'Dessert 4', price: '$8', image: cheesecake },
+    { name: 'Dessert 5', price: '$9', image: cheesecake },
+    { name: 'Dessert 6', price: '$7', image: cheesecake },
+    { name: 'Dessert 7', price: '$9', image: cheesecake },
+    { name: 'Dessert 8', price: '$7', image: cheesecake },
 
   ],
   snacks: [
-    { name: 'Snack 1', price: '$4', image: '/snacks.jpeg' },
-    { name: 'Snack 2', price: '$5', image: '/snacks.jpeg' },
-    { name: 'Snack 3', price: '$3', image: '/snacks.jpeg' },
-    { name: 'Snack 4', price: '$4', image: '/snacks.jpeg' },
-    { name: 'Snack 5', price: '$5', image: '/snacks.jpeg' },
-    { name: 'Snack 6', price: '$3', image: '/snacks.jpeg' },
-    { name: 'Snack 7', price: '$5', image: '/snacks.jpeg' },
-    { name: 'Snack 8', price: '$3', image: '/snacks.jpeg' },
+    { name: 'Snack 1', price: '$4', image: snacks },
+    { name: 'Snack 2', price: '$5', image: snacks },
+    { name: 'Snack 3', price: '$3', image: snacks },
+    { name: 'Snack 4', price: '$4', image: snacks },
+    { name: 'Snack 5', price: '$5', image: snacks },
+    { name: 'Snack 6', price: '$3', image: snacks },
+    { name: 'Snack 7', price: '$5', image: snacks },
+    { name: 'Snack 8', price: '$3', image: snacks },
   ],
 };
 
@@ -61,6 +65,7 @@ const CategoryPage = () => {
         <div className="item-container">
           {items.map((item, index) => (
             <div key={index} className="item-card">
+                {/* <img src={require('../Images/mango.jpeg')} /> */}
               <img src={item.image} alt={item.name} className="item-image" />
               <h2>{item.name}</h2>
               <p>{item.price}</p>
